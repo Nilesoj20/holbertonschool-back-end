540c8fb3ab50c3bb1f745883876fdc0e71749e3b
Nilesoj20 <jvrn2014@gmail.com>
2023-08-23 16:19:18 -0500

    feat: Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Export data in CSV format"""
+
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    """using REST API Placeholder through parameter"""
+    parametro_id = sys.argv[1]
+
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={parametro_id}"
+    url_nombre = f"https://jsonplaceholder.typicode.com/users/{parametro_id}"
+
+    respuesta = requests.get(url)
+    respuesta_nombre = requests.get(url_nombre)
+
+    total_tarea = respuesta.json()
+    informacion_empleado = respuesta_nombre.json()
+    nombre_empleado = informacion_empleado.get("name")
+
+    tarea_completadas = []
+    for tarea in total_tarea:
+        if tarea["completed"]:
+            tarea_completadas.append(tarea)
+    cantidad_tarea_completada = len(tarea_completadas)
+    cantidad_total_tarea = len(total_tarea)
+
+    formato_archivo = f"{parametro_id}.csv"
+    with open(formato_archivo, mode='w', newline="") as file:
+        formato = ["USER_ID",
+                   "USERNAME",
+                   "TASK_COMPLETED_STATUS",
+                   "TASK_TITLE"]
+        writer = csv.DictWriter(file, fieldnames=formato)
+
+        for task in total_tarea:
+            writer.writerow({
+                "USER_ID": f'{parametro_id}',
+                "USERNAME": f'{nombre_empleado}',
+                "TASK_COMPLETED_STATUS": f'{task.get("completed")}',
+                "TASK_TITLE": f'{task.get("title")}'
+            })
