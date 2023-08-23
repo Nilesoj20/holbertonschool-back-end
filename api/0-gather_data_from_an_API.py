67fd41e3e9a6e2946fc7084523f16614b94a49cc
Nilesoj20 <jvrn2014@gmail.com>
2023-08-23 00:03:05 -0500

    fix: changing syntax to correct error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,10 +16,11 @@ def inf_empleados():
     url_nombre = f"https://jsonplaceholder.typicode.com/users/{parametro_id}"
 
     respuesta = requests.get(url)
-    if respuesta.status_code == 200:
-        "deserializacion del archivo json optenido por el metodo get a la url"
+    respuesta_nombre = requests.get(url_nombre)
+    if respuesta.status_code == 200 and respuesta_nombre.status_code == 200:
         total_tarea = respuesta.json()
-        if not total_tarea:
+        informacion_empleado = respuesta_nombre.json()
+        if not total_tarea or not informacion_empleado:
             print("error in deserialization")
             return
         tarea_completadas = []
@@ -28,12 +29,7 @@ def inf_empleados():
                 tarea_completadas.append(tarea)
         cantidad_tarea_completada = len(tarea_completadas)
         cantidad_total_tarea = len(total_tarea)
-    else:
-        print("status error ")
 
-    respuesta_nombre = requests.get(url_nombre)
-    if respuesta_nombre.status_code == 200:
-        informacion_empleado = respuesta_nombre.json()
         if "name" in informacion_empleado:
             nombre_empleado = informacion_empleado.get("name")
         print("Employee {} is done with tasks({}/{}):"
@@ -44,7 +40,7 @@ def inf_empleados():
         for tarea in tarea_completadas:
             print("\t {}".format(tarea.get("title")))
     else:
-        print("status error name")
+        print("status error")
 
 
 if __name__ == '__main__':
